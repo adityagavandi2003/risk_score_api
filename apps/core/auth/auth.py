4b2595f13201cbb2eb5c8fdc296f77db0852171c
Aditya Gavandi <adityagavandi1998@gmail.com>
2025-09-08 10:39:01 +0530

    Update auth.py

--- apps/core/auth/auth.py
+++ apps/core/auth/auth.py
@@ -4,6 +4,6 @@ from rest_framework.exceptions import AuthenticationFailed
 class HeaderApiKeyAuthentication(BaseAuthentication):
     def authenticate(self, request):
         api_key = request.headers.get("X-API-Key")
-        if api_key == "secret123":  # your fixed key
-            return (None, None)  # no user, no auth object
+        if api_key == "secret123":  #fixed key
+            return (None, None) 
         raise AuthenticationFailed("Invalid or missing API Key")
